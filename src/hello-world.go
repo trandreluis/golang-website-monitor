87059fd5fcba8c4b75359adab36f3e01712de86e
Andre Luis <tr.andreluis@gmail.com>
2021-10-28 04:02:28 -0300

    Reading user input data and implementing conditions

--- src/hello-world.go
+++ src/hello-world.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
 	nome := "André Luís"
-	idade := 25
 	versao := 1.1
-	fmt.Println("Olá, sr.", nome, "sua idade é", idade)
+	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
 
-	fmt.Println("O tipo da variável nome é", reflect.TypeOf(nome))
-	fmt.Println("O tipo da variável idade é", reflect.TypeOf(idade))
-	fmt.Println("O tipo da variável versao é", reflect.TypeOf(versao))
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do programa")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O endereço da minha variável comando é", &comando)
+
+	if comando == 1 {
+		fmt.Println("A opção 1 foi escolhida")
+	} else if comando == 2 {
+		fmt.Println("A opção 2 foi escolhida")
+	} else if comando == 0 {
+		fmt.Println("A opção 0 foi escolhida")
+	}
 }
