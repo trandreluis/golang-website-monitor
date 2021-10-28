0154b4bcea4e58c9a730e314576b6f2018765179
Andre Luis <tr.andreluis@gmail.com>
2021-10-28 15:04:52 -0300

    Implementing functions and system exit

--- src/hello-world.go
+++ src/hello-world.go
@@ -2,20 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	nome := "André Luís"
-	versao := 1.1
-	fmt.Println("Olá, sr.", nome)
-	fmt.Println("Este programa está na versão", versao)
 
-	fmt.Println("1 - Iniciar monitoramento")
-	fmt.Println("2 - Exibir logs")
-	fmt.Println("0 - Sair do programa")
+	exibeIntroducao()
+	exbibeMenu()
 
-	var comando int
-	fmt.Scan(&comando)
+	comando := leComando()
 
 	switch comando {
 	case 1:
@@ -24,8 +19,31 @@ func main() {
 		fmt.Println("Exibindo logs")
 	case 0:
 		fmt.Println("Saindo do programa")
+		os.Exit(0)
 	default:
 		fmt.Println("Comando desconhecido")
+		os.Exit(-1)
 	}
 
+}
+
+func exibeIntroducao() {
+	nome := "André Luís"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func exbibeMenu() {
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+
+	return comandoLido
 }
