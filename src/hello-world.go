b8db836deeaf4afd885d1d4d795d38a31500ed47
Andre Luis <tr.andreluis@gmail.com>
2021-10-28 03:48:45 -0300

    Inferencia de tipo e declaracao de variavel com atribuicao rapida (sem o var)

--- src/hello-world.go
+++ src/hello-world.go
@@ -1,10 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	var nome string = "André Luís"
-	var versao float32 = 1.1
-	fmt.Println("Olá, sr.", nome)
+	nome := "André Luís"
+	idade := 25
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome, "sua idade é", idade)
 	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("O tipo da variável nome é", reflect.TypeOf(nome))
+	fmt.Println("O tipo da variável idade é", reflect.TypeOf(idade))
+	fmt.Println("O tipo da variável versao é", reflect.TypeOf(versao))
 }
